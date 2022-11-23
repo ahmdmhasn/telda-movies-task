c5bbfc2a06251cfb4432b4e78b9de08da7abd7dd
Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
2022-11-23 23:52:15 +0200

    Enhance url request creation

--- TeldaMovies/Networking/TMDBRequest.swift
+++ TeldaMovies/Networking/TMDBRequest.swift
@@ -12,10 +12,23 @@ import Foundation
 struct TMDBRequest {
     let path: String
     let method: String
-    
+    let parameters: [String: String]
+        
+    init(path: String, method: String, parameters: [String: String] = [:]) {
+        self.path = path
+        self.method = method
+        self.parameters = parameters
+    }
+}
+
+// MARK: URLRequest Generator
+//
+extension TMDBRequest {
     func asURLRequest() -> URLRequest {
         var components = URLComponents(string: Constants.baseUrl + path)!
-        components.queryItems = [URLQueryItem(name: "api_key", value: Constants.apiKey)]
+        components.queryItems = parameters
+            .merging(["api_key": Constants.apiKey], uniquingKeysWith: { (current, _) in current })
+            .map { URLQueryItem(name: $0.key, value: $0.value) }
         
         var request = URLRequest(url: components.url!)
         request.httpMethod = method
