313e4ad54f2121f53366b8644134eb42d45f90b3
Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
2022-11-25 22:05:01 +0200

    Configure movie overview

--- TeldaMovies/Modules/MovieDetails/Views/MovieOverviewTableViewCell/MovieOverviewTableViewCell.swift
+++ TeldaMovies/Modules/MovieDetails/Views/MovieOverviewTableViewCell/MovieOverviewTableViewCell.swift
@@ -11,18 +11,23 @@ class MovieOverviewTableViewCell: UITableViewCell {
 
     // MARK: Outlets
     
+    @IBOutlet private weak var movieImageView: UIImageView!
+    @IBOutlet private weak var movieDescription: UILabel!
     
     // MARK: Lifecycle
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        movieDescription.textColor = .darkGray
+        movieDescription.font = .preferredFont(forTextStyle: .callout)
     }
     
     // MARK: Configuration
 
     func configure(_ viewModel: ViewModel) {
-        // TODO:
+        movieImageView.setImage(viewModel.imagePath)
+        movieDescription.text = viewModel.overview
     }
 }
 
@@ -31,6 +36,7 @@ class MovieOverviewTableViewCell: UITableViewCell {
 extension MovieOverviewTableViewCell {
     struct ViewModel {
         let title: String
+        let imagePath: String
         let overview: String
         let tagline: String
         let revenue: String
@@ -39,6 +45,8 @@ extension MovieOverviewTableViewCell {
         
         init(movie: MovieEntity) {
             self.title = movie.title ?? String()
+            self.imagePath = TMDBImageBuilder(path: movie.posterPath ?? String(), type: .original)
+                .absolutePath()
             self.overview = movie.overview ?? String()
             self.tagline = movie.tagline ?? String()
             self.revenue = movie.revenue.map { String($0) } ?? String()
